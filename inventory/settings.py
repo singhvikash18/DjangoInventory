8e13d0329a6d4460a1e97733c1f2631f99c86c98
Vikash Singh <vikashkumarsingh1811@gmail.com>
2021-05-10 18:14:12 +0530

    test

--- inventory/settings.py
+++ inventory/settings.py
@@ -86,9 +86,8 @@ DATABASES = {
         'PASSWORD': '1234',
         'HOST': '127.0.0.1',
         'PORT': '3306',
-        'OPTIONS': {
           'autocommit': True,
-        },
+        
     #'default':env.db(),
     
     }
